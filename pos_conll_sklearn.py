4480afd218159f1401403c881d91ff0cf268f514
abhibha gupta <34295094+abhibha1807@users.noreply.github.com>
2020-06-16 16:02:03 +0530

    add files
    working on conll_dataset

---
+++ pos_conll_sklearn.py
@@ -0,0 +1,149 @@
+
+import nltk
+import creme
+import pandas as pd
+import pprint 
+from nltk import word_tokenize, pos_tag
+from collections import Counter
+import matplotlib.pyplot as plt 
+from creme import tree
+from creme.tree import DecisionTreeClassifier
+from creme import metrics
+from creme import model_selection
+from creme import compose
+from datetime import datetime, timedelta 
+import sklearn
+from sklearn.metrics import classification_report
+file1 = open('/Users/abhibhagupta/Desktop/TCS/version_control_TCS_POS_exploration/datasets/conll_dataset_pos/pos.train.txt', 'r') 
+Lines = file1.readlines() 
+#print(type(Lines))
+l=[]
+t=[]
+tagged_sentences=[]
+for i in range(len(Lines)):
+    l=Lines[i].split(' ')
+    if l[0]!='\n':
+        l.pop(2)
+        t.append(tuple(l))
+    else:
+        tagged_sentences.append(t)
+        t=[]
+# print(dataset[0])
+# print(len(dataset))
+
+
+# create features 
+def features(sentence, index):
+    """ sentence: [w1, w2, ...], index: the index of the word """
+    return {
+        'word': sentence[index],
+        'is_first': index == 0,
+        'is_last': index == len(sentence) - 1,
+        'is_capitalized': sentence[index][0].upper() == sentence[index][0],
+        'is_all_caps': sentence[index].upper() == sentence[index],
+        'is_all_lower': sentence[index].lower() == sentence[index],
+        'prefix-1': sentence[index][0],
+        'prefix-2': sentence[index][:2],
+        'prefix-3': sentence[index][:3],
+        'suffix-1': sentence[index][-1],
+        'suffix-2': sentence[index][-2:],
+        'suffix-3': sentence[index][-3:],
+        'prev_word': '' if index == 0 else sentence[index - 1],
+        'next_word': '' if index == len(sentence) - 1 else sentence[index + 1],
+        'has_hyphen': '-' in sentence[index],
+        'is_numeric': sentence[index].isdigit(),
+        'capitals_inside': sentence[index][1:].lower() != sentence[index][1:]
+    }
+ 
+def untag(tagged_sentence):
+    return [w for w, t in tagged_sentence]
+
+cutoff = int(.75 * len(tagged_sentences))
+training_sentences = tagged_sentences[:cutoff]
+test_sentences = tagged_sentences[cutoff:]
+ 
+print (len(training_sentences) )  # 2935
+print (len(test_sentences)   )      # 979
+ 
+def transform_to_dataset(tagged_sentences):
+    X, y = [], []
+ 
+    for tagged in tagged_sentences:
+        for index in range(len(tagged)):
+            X.append(features(untag(tagged), index))
+            y.append(tagged[index][1])
+ 
+    return X, y
+ 
+X, y = transform_to_dataset(training_sentences)
+
+ 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import Pipeline
+ 
+clf = Pipeline([
+    ('vectorizer', DictVectorizer(sparse=False)),
+    ('classifier', DecisionTreeClassifier(criterion='entropy'))
+])
+
+# print(dir(clf.score))
+clf.fit(X[:10000], y[:10000])   # Use only the first 10K samples if you're running it multiple times. It takes a fair bit :)
+
+print ('Training completed')
+ 
+X_test, y_test = transform_to_dataset(test_sentences)
+
+y_pred=clf.predict(X_test)
+
+print(type(y_pred),y_pred[0])
+print ("F1:", classification_report(y_test,y_pred))
+ 
+#F1:               precision    recall  f1-score   support
+
+           #       1.00      1.00      1.00        18
+#            $       1.00      0.99      0.99       421
+#           ''       0.93      1.00      0.96       335
+#            (       1.00      1.00      1.00        71
+#            )       1.00      1.00      1.00        72
+#            ,       1.00      1.00      1.00      2685
+#            .       1.00      1.00      1.00      2211
+#            :       1.00      0.95      0.97       286
+#           CC       1.00      0.99      0.99      1338
+#           CD       0.99      0.98      0.98      2220
+#           DT       0.99      0.98      0.98      4433
+#           EX       0.83      0.87      0.85        46
+#           FW       0.00      0.00      0.00         3
+#           IN       0.95      0.96      0.95      5684
+#           JJ       0.75      0.69      0.72      3114
+#          JJR       0.62      0.66      0.64       205
+#          JJS       0.82      0.80      0.81        97
+#           MD       0.98      0.98      0.98       569
+#           NN       0.80      0.83      0.82      7518
+#          NNP       0.91      0.97      0.94      5432
+#         NNPS       0.62      0.14      0.23       116
+#          NNS       0.91      0.93      0.92      3412
+#          PDT       0.86      0.40      0.55        15
+#          POS       0.92      0.95      0.93       479
+#          PRP       0.96      0.91      0.93       830
+#         PRP$       0.99      0.95      0.97       454
+#           RB       0.84      0.82      0.83      1641
+#          RBR       0.83      0.24      0.37        79
+#          RBS       0.98      0.95      0.97        59
+#           RP       1.00      0.21      0.34        29
+#           TO       1.00      1.00      1.00      1308
+#           VB       0.70      0.69      0.69      1508
+#          VBD       0.81      0.87      0.84      1799
+#          VBG       0.77      0.87      0.82       815
+#          VBN       0.77      0.67      0.72      1204
+#          VBP       0.73      0.68      0.70       658
+#          VBZ       0.85      0.71      0.77      1075
+#          WDT       0.94      0.66      0.78       244
+#           WP       0.88      0.95      0.91       117
+#          WP$       0.00      0.00      0.00         9
+#          WRB       0.99      0.93      0.96       106
+#           ``       1.00      1.00      1.00       342
+
+#    micro avg       0.89      0.89      0.89     53057
+#    macro avg       0.85      0.79      0.81     53057
+# weighted avg       0.89      0.89      0.89     53057
---
+++ preprocess_conll.py
@@ -0,0 +1,193 @@
+
+import nltk
+import creme
+import pandas as pd
+import pprint 
+from nltk import word_tokenize, pos_tag
+from collections import Counter
+import matplotlib.pyplot as plt 
+from creme import tree
+from creme.tree import DecisionTreeClassifier
+from creme import metrics
+from creme import model_selection
+from creme import compose
+from datetime import datetime, timedelta 
+
+
+
+# create features 
+def features(sentence, index):
+    """ sentence: [w1, w2, ...], index: the index of the word """
+    return {
+        'word': sentence[index],
+        'is_first': index == 0,
+        'is_last': index == len(sentence) - 1,
+        'is_capitalized': sentence[index][0].upper() == sentence[index][0],
+        'is_all_caps': sentence[index].upper() == sentence[index],
+        'is_all_lower': sentence[index].lower() == sentence[index],
+        'prefix-1': sentence[index][0],
+        'prefix-2': sentence[index][:2],
+        'prefix-3': sentence[index][:3],
+        'suffix-1': sentence[index][-1],
+        'suffix-2': sentence[index][-2:],
+        'suffix-3': sentence[index][-3:],
+        'prev_word': '' if index == 0 else sentence[index - 1],
+        'next_word': '' if index == len(sentence) - 1 else sentence[index + 1],
+        'has_hyphen': '-' in sentence[index],
+        'is_numeric': sentence[index].isdigit(),
+        'capitals_inside': sentence[index][1:].lower() != sentence[index][1:]
+    }
+ 
+def untag(tagged_sentence):
+    return [w for w, t in tagged_sentence]
+
+def transform_to_dataset(tagged_sentences):
+    X, Y = [], []
+ 
+    for tagged in tagged_sentences:
+        for index in range(len(tagged)):
+            X.append(features(untag(tagged), index))
+            Y.append(tagged[index][1])
+
+
+    X=X[0:10000]
+    print(set(Y))
+
+    X_y=[]
+    for i in range(len(X)):
+    	X_y.append((X[i],Y[i]))
+    return(X_y)
+ 
+
+# get dictionary mapping of word and count
+file1 = open('/Users/abhibhagupta/Desktop/TCS/version_control_TCS_POS_exploration/datasets/conll_dataset_pos/pos.train.txt', 'r') 
+Lines = file1.readlines() 
+#print(type(Lines))
+l=[]
+t=[]
+tagged_sentences=[]
+for i in range(len(Lines)):
+	l=Lines[i].split(' ')
+	if l[0]!='\n':
+		l.pop(2)
+		t.append(tuple(l))
+	else:
+		tagged_sentences.append(t)
+		t=[]
+# print(dataset[0])
+# print(len(dataset))
+
+
+X, Y = [], []
+
+for tagged in tagged_sentences:
+    for index in range(len(tagged)):
+        X.append(features(untag(tagged), index))
+
+
+df=pd.DataFrame(X)
+
+cols=['word','prev_word','next_word','prefix-1','prefix-2','prefix-3','suffix-1','suffix-2','suffix-3']
+d={}
+for col_name in cols:
+	q={}
+	q=Counter(list(df[col_name]))
+	d={**d,**q}
+        
+#print(d)
+
+def preprocess(x):
+	l=['capitals_inside','has_hyphen','is_all_caps','is_all_lower','is_capitalized', 'is_first', 'is_last','is_numeric']
+	for i in l:
+		if x[i]==True:
+			x[i]=1.0
+		else:
+			x[i]=0.0
+
+	return(x)
+
+# converting text into numbers based on there frequencies of occurence
+def preprocess1(x):
+	cols=['word','prev_word','next_word','prefix-1','prefix-2','prefix-3','suffix-1','suffix-2','suffix-3']
+
+	for i in cols:
+		x[i]=d[x[i]]
+	
+	return(x)
+
+
+def main():
+
+	# obtain the tagged sentences
+	
+	X_y= transform_to_dataset(tagged_sentences)
+
+	metric = metrics.F1()
+
+	# compose pipeline for preprocessing and predictions
+	model = compose.Pipeline(
+      ('preprocess', preprocess),
+      ('preprocess1', preprocess1),
+      ('tree', DecisionTreeClassifier(patience=10,confidence=1e-2,criterion='entropy',max_depth=100,min_child_samples=1))
+      )
+
+	# print(type(model))
+	# print(model)
+
+	# for x, y in X_y:
+	# 	y_pred = model.predict_proba_one(x)
+	# 	metric = metric.update(y, y_pred)
+	# 	model = model.fit_one(x, y)
+	# print(metric)
+
+	print(model_selection.progressive_val_score(X_y=X_y, model=model, metric=metric,print_every=10000))	
+
+	
+if __name__ == '__main__':
+	main()
+
+
+# results (first 10k instances)
+# [1,000] F1: 0.107107
+# [2,000] F1: 0.122561
+# [3,000] F1: 0.128376
+# [4,000] F1: 0.126532
+# [5,000] F1: 0.125025
+# [6,000] F1: 0.127188
+# [7,000] F1: 0.126304
+# [8,000] F1: 0.126891
+# [9,000] F1: 0.123569
+# [10,000] F1: 0.123712
+
+# results (first 10k instances (delay=2))
+# [1,000] F1: 0.113226
+# [2,000] F1: 0.127127
+# [3,000] F1: 0.131421
+# [4,000] F1: 0.128814
+# [5,000] F1: 0.126851
+# [6,000] F1: 0.12871
+# [7,000] F1: 0.127608
+# [8,000] F1: 0.128032
+# [9,000] F1: 0.124583
+# [10,000] F1: 0.124625
+# F1: 0.124625
+
+
+# results (whole dataset)
+# [10,000] F1: 0.123712
+# [20,000] F1: 0.126456
+# [30,000] F1: 0.127571
+# [40,000] F1: 0.128828
+# [50,000] F1: 0.134103
+# [60,000] F1: 0.137902
+# [70,000] F1: 0.142802
+# [80,000] F1: 0.148102
+# [90,000] F1: 0.153324
+# [100,000] F1: 0.158822
+# F1: 0.159305
+
+# results whole dataset conll
+#F1: 0.220167
+
+# results combined
+# almost same as above
